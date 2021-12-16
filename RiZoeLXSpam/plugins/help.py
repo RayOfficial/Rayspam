95671a8b34c9cc158aaa26189e342d67ad861900
RayOfficial <96227061+RayOfficial@users.noreply.github.com>
2021-12-16 15:43:45 +0530

    Add files via upload

---
+++ RiZoeLXSpam/plugins/help.py
@@ -0,0 +1,44 @@
+from .. import Riz, SUDO_USERS
+from telethon import events, Button
+from telethon.tl.custom import button
+from time import time
+from datetime import datetime
+    
+HELP_PIC = "https://telegra.ph/spambot-12-16"
+
+Riz_Help = "🔥 𝗥𝗜𝗭𝗢𝗘𝗟 𝗫 𝗦𝗣𝗔𝗠 🔥\n\n"
+ 
+Riz_Help += f"__ᴄᴍᴅs ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ ʀɪᴢᴏᴇʟ x sᴘᴀᴍ__\n\n"
+
+Riz_Help += f" ↧ 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙲𝙼𝙳𝚂 ↧\n\n"
+
+Riz_Help += f" `.ping` - to check ping\n `.alive` - to check bot alive/version (only main userbot will reply)\n .`restart` - to restart all spam bots\n\n"
+ 
+Riz_Help += f" ↧ 𝙻𝙴𝙰𝚅𝙴 𝙲𝙼𝙳 ↧\n\n"
+
+Riz_Help += f" `.leave` - to leave public/private channel/groups\n\n"
+ 
+Riz_Help += f" ↧ 𝚂𝙿𝙰𝙼 𝙲𝙼𝙳𝚂 ↧\n\n"
+
+Riz_Help += f" `.raid` - to raid\n `.replyraid` - to active reply raid\n `.dreplyraid` - to de-active reply raid\n `.spam` - this cmd use for Normal spam\n `.bigspam` - this cmd use for big spam\n `.uspam` - this cmd use for unlimited Spam until You restart the bots!!\n `.delayspam` - this cmd use for delay spam\n\n"
+ 
+Riz_Help += f"ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏ.\n\n"
+
+Riz_Help += f"© @Funtwenty4 | @Innocent_Gaurav\n"
+
+
+@Riz.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Riz.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Riz_Help,
+                                  buttons=[
+        [
+        Button.url("ᴀʟʟ ᴄᴍᴅs", "https://telegra.ph/spambot-12-16-2")
+        ],
+        [
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/FunTwenty4")
+        ] 
+        ]
+        )                                                         
---
+++ RiZoeLXSpam/plugins/start.py
@@ -0,0 +1,170 @@
+import asyncio
+import os
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon import events, Button
+from telethon.tl.custom import button
+from .. import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10, ALIVE_PIC
+
+RIZ_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/spambot-12-16"
+
+
+Riz_Button = [
+        [
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/Innocent_Gaurav"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/funtwenty4")
+        ],
+        [
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/RayOfficial/RayVersion2")
+        ]
+        ]
+
+
+@Riz.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/Funtwenty4)**"
+       await Riz.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)
+                
+@Riz2.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz2.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/Funtwenty4)**"
+       await Riz2.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)      
+ 
+@Riz3.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz3.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/Funtwenty4)**"
+       await Riz3.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)
+                
+@Riz4.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz4.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/Funtwenty4)**"
+       await Riz4.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)  
+
+@Riz5.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz5.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/FunTwenty4)**"
+       await Riz5.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)
+
+@Riz6.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz6.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/funtwenty4)**"
+       await Riz6.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)       
+
+@Riz7.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz7.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id 
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/funtwenty4)**"
+       await Riz7.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)
+
+@Riz8.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz8.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/funtwenty4)**"
+       await Riz8.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)
+                
+@Riz9.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz9.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/Funtwenty4)**"
+       await Riz9.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)
+                
+@Riz10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await Riz10.get_me()
+       bot_id = RizBot.first_name
+       bot_username = RizBot.username
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       firstname = replied_user.user.first_name
+       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/funtwenty4)**"
+       await Riz10.send_file(TheRiZoeL,
+                RIZ_IMG,
+                caption=Rizmsg, 
+                buttons=Riz_Button)                
